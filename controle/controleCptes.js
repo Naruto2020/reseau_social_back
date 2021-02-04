555f57380f33fb7ac3c70c634122b6ad7ea72c51
Naruto2020 <guyfredy@gmail.com>
2021-02-04 19:23:22 +0100

    gestion demande amis 13

--- controle/controleCptes.js
+++ controle/controleCptes.js
@@ -313,7 +313,7 @@ router.post("/users", /*upload.single("photo"),*/ (req, res)=>{
         console.log("erreur de cryptage" + JSON.stringify(err, undefined, 2));
     }else{*/
 
-      const  { nom, prenom, username, mail, password, age, coordonnees, genre, preferences, niveau, presentation, amis} = req.body;
+      const  { nom, prenom, username, mail, password, age, coordonnees, genre, preferences, niveau, presentation, followings, followers} = req.body;
       User.findOne({username}, (err, user)=>{
         if(err){
           res.status(500).json({message : {msgBody : "une erreur c'est produite", msgError:true}});
@@ -322,7 +322,7 @@ router.post("/users", /*upload.single("photo"),*/ (req, res)=>{
           res.status(400).json({message : {msgBody : "pseudo déja utilisé", msgError:true}});
 
         }else{
-          const newUser = new User({nom, prenom, username, mail, password, age, coordonnees, genre, preferences, niveau, presentation, amis});
+          const newUser = new User({nom, prenom, username, mail, password, age, coordonnees, genre, preferences, niveau, presentation, followings, followers});
 
           //enregistrement du profil 
           newUser.save((err,doc)=>{
@@ -428,6 +428,8 @@ router.put("/users/:id" , (req, res, next) => {
           preferences:req.body.preferences,
           niveau : req.body.niveau,
           presentation : req.body.presentation,
+          followings : req.body.followings,
+          followers : req.body.followers
         };
 
         User.findByIdAndUpdate(
@@ -718,6 +720,78 @@ router.get('/:nom', function(req, res) {
 
 // Ajout amis 
 
+/*router.patch('/follow/:username', (req, res, next)=>{
+  async.waterfall([
+    function(done){
+      // ajout liste followings
+      User.findOne({username:req.params.username}, (err, docs)=>{
+        console.log("ut qui invite",req.params.username);
+        if(err){
+          res.status(400).json({message : {msgBody : "utilisateur non trouvé .", msgError:true}});
+        }
+        //followings = req.body.userToFolow;
+        //user.amis.notifs = Date.now();
+        const user = new User
+            
+        user.followings.push( req.body.userToFollow);
+        console.log("qui est ce ...",req.body.userToFollow);
+        user.save((err, user)=>{
+          if(err){
+            res.status(500).json({message : {msgBody : "une erreur c'est produite", msgError:true}});
+          } 
+          if(user){
+            res.status(200).json({message:"modifications bien enregistrées"});
+          }
+        });
+        
+      });
+
+      // ajout liste followers
+      User.findOne({username:req.body.userToFollow}, (err, user)=>{
+        if(!user){
+          res.status(400).json({message : {msgBody : "utilisateur non trouvé .", msgError:true}});
+        }
+        user.followers.push(req.params.username);
+        user.save(err=>{
+          done(err, user);
+        });
+      });
+    },
+    
+    function( user, done){
+      var smtpTransport = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+          user:details.mail, // generated ethereal user
+          pass: details.password // generated ethereal password
+        },
+      });
+      let mailOptions = {
+        from: '"Swap-It 👻" ghpower409@gmail.com', // address email emettrice
+        to: user.mail, // address email receptrice
+        subject: "wellcome to Swap-It 👻 ✔", // Sujet 
+        html: `<h1>Bonjour  ${user.username} </h1><br/>
+        <h4>L'utilisateur ${req.params.username}  vous a ajouter à ça liste d'amis </h4>`,
+         
+      };
+      smtpTransport.sendMail(mailOptions, (err)=>{
+        res.json({message: 'message recu !!!'});
+        done(err);
+      });
+    }
+
+  ], function(err){
+    if(err){
+      return next(err);
+    }
+
+  });
+});*/
+
+// Ajout amis *
+
 router.patch('/follow/:username', (req, res, next)=>{
   async.waterfall([
     function(done){
@@ -785,7 +859,7 @@ router.patch('/follow/:username', (req, res, next)=>{
     }
 
   });
-});
+}); 
 
 // suppression d'amis 
 
